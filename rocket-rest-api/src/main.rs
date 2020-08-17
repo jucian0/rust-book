bd7222b32ae69403fe85d07835d25cbbe4a96b0c
Jucian0 <juciano@outlook.com.br>
2020-08-17 20:26:32 -0300

    init rocket example

--- rocket-rest-api/src/main.rs
+++ rocket-rest-api/src/main.rs
@@ -1,12 +1,26 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+use rocket_contrib::json::Json;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
 
 #[get("/")]
 fn index()-> &'static str{
     "Hello, World!"
 }
 
+#[get("/user")]
+fn user()-> Json<User> {
+    Json(User {name:"Juciano"})
+}
+
+
 fn main(){
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, user]).launch();
 }
